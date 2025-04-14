37af1bd4411e0dbdac50ee72a545010c1dd01186
Calum Murray <145492970+RidSoftware@users.noreply.github.com>
2025-04-14 13:29:40 +0100

    Update sim.py

--- sim.py
+++ sim.py
@@ -5,5 +5,15 @@ width = 10, height = 10
 changeInTime = 1
 
 class Particle:
-  def initiallize(self, mass = 1 radius = 1, position, velocity):
+  def initialise(self, mass = 1 radius = 1, position, velocity):
     self.position = np.array(position, dtype = float)
+    self.velocity = np.array(velocity, dtype=float)
+    self.radius = radius
+    self.mass = mass
+  
+  def update(self):
+    self.velocity += gravity * changeInTime
+    self.position += self.velocity * changeInTime
+
+  def wallCollision(self):
+    
